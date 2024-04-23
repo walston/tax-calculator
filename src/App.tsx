458bf7fd0f2894754f220afc85cacc5b8d40cda1
ratherblue <ratherblue@gmail.com>
2024-04-23 16:02:34 -0700

    test routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './Layout'
+import Index from './pages/Index'
 import FederalIncomeTax from './pages/calculators/FederalIncomeTax.tsx'
 import NoPage from './pages/404'
 
@@ -8,7 +9,8 @@ const App = () => {
 <BrowserRouter>
   <Routes>
     <Route path="/" element={<Layout/>}>
-      <Route index element={<FederalIncomeTax/>}/>
+      <Route index element={<Index/>}/>
+      <Route path="federal-income-tax-calculator" element={<FederalIncomeTax/>}/>
     </Route>
     <Route path="*" element={<NoPage/>}/>
   </Routes>
