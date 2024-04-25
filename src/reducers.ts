fd78504a9984db27b543897b5c1b189f4ced71ff
Nathan Walston <nathanielwalston@gmail.com>
2024-04-25 07:17:03 -0700

    fix synchronization bug

--- src/reducers.ts
+++ src/reducers.ts
@@ -25,14 +25,17 @@ const formReducer = (state = initialState, action: UpdateTaxDataAction) => {
   const taxForm = cloneDeep(state.taxForm)
   switch (action.type) {
     case 'UPDATE_TAX_DATA':
+      // action.payload is perfectly mapped to the taxForm data-structure
+      // therefore we safely update w/out checks
+      Object.assign(taxForm, action.payload);
+
+      // now that taxForm is synced with the NEXT state being processed
+      // update deductions
       if (taxForm.useStandardDeduction) {
-        taxForm.deductions = (taxForm.filingStatus === 'single') ? federalTax.single.standardDeduction.toString() : federalTax.mfj.standardDeduction.toString()
+        taxForm.deductions = federalTax[taxForm.filingStatus as 'mfj' | 'single'].standardDeduction.toString()
       }
 
-      return {
-        ...state,
-        taxForm: { ...taxForm, ...action.payload }
-      };
+      return { ...state, taxForm };
     default:
       return state;
   }
